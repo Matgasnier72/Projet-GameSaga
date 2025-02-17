2c6d43e791691a4f11330cdcce377ad0a3dae1ea
Matgasnier72 <mat72.multi@gmail.com>
2025-02-17 09:53:01 +0100

    saut de ligne

--- GameSagaFront/src/_models/Articles.ts
+++ GameSagaFront/src/_models/Articles.ts
@@ -1,9 +1,13 @@
 export interface Article {
-    titre: string;
-  }
-  
-  export interface SearchResponse {
-    data: {
-      post: Article[];
-    };
-  }
+  id?: number;
+  titre: string;
+  created_at: string;
+  contenu: string;
+  note: number;
+}
+
+export interface SearchResponse {
+  data: {
+    post: Article[];
+  };
+}
--- GameSagaFront/src/views/Articles.vue
+++ GameSagaFront/src/views/Articles.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
 import { ref, watch, onMounted } from 'vue';
-import { searchArticle } from '@/_services/ArticleService'; // API call function
+import { getArticles } from '@/_services/ArticleService'; // API call function
 import SearchBar from '../components/SearchBar.vue';
 
 // Reactive Variables
-const articles = ref<{ titre: string; created_at: string; contenu: string; note: number }[]>([]);
+const articles = ref<{ id?:number;titre: string; created_at: string; contenu: string; note: number }[]>([]);
 const error = ref<string | null>(null);
 const titre = ref<string>(''); // Search input value
 
 // Function to Fetch Articles
 const fetchArticles = async () => {
   try {
-    const response = await searchArticle(titre.value); // Assuming API function takes a title parameter
-    articles.value = response.data; // Adjust based on API response format
+    const response = await getArticles(); // Assuming API function takes a title parameter
+    articles.value = response; // Adjust based on API response format
     error.value = null;
   } catch (err) {
     error.value = "Erreur lors de la récupération des articles.";
@@ -33,12 +33,11 @@ onMounted(fetchArticles);
 
       <!-- Search Bar -->
       <SearchBar v-model="titre" />
-
       <!-- Error Message -->
       <p v-if="error" class="text-danger">{{ error }}</p>
 
       <!-- Articles -->
-      <div v-for="(article, index) in articles" :key="index" class="carte container d-none d-lg-block my-5">
+      <div v-for="article in articles" :key="article.id" class="carte container d-none d-lg-block my-5">
         <div class="row">
           <div class="col carte py-3 pt-1">
             <img src="../assets/valve-software-504f0b01a27a2.webp" class="vignette text-truncate" alt="Article Image" />
